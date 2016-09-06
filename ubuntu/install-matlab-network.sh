4faf3901d7dd65e94faceb007ccf5503b2418367
tal.regev@gmail.com <tal.regev@gmail.com>
2016-09-06 12:14:37 +0300

    update matlab installation. add matlab installation from network.

--- ubuntu/install-matlab-network.sh
+++ ubuntu/install-matlab-network.sh
@@ -3,14 +3,14 @@
 #from https://help.ubuntu.com/community/MountIso
 
 sudo mkdir -p /media/matlab
-sudo mount -o loop ~/iso-matlab/R2014b_glnxa64.iso /media/matlab -o noauto
+sudo mount 132.68.1.41:/ccnfs/cccd/matlab /media/matlab
 sudo mkdir -p /tmp/network
 cat << EOF > network.lic
 SERVER lm.technion.ac.il 001b24e02dd5 1700
 USE_SERVER
 EOF
 sudo mv network.lic /tmp/network
-cd /media/matlab/
+cd /media/matlab
 sudo ./install
-sleep 2 
+sleep 2
 sudo umount /media/matlab
