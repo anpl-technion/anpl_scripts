4186e1508cd5e5f149afee7e6c52db36f69f5a55
tal.regev@gmail.com <tal.regev@gmail.com>
2016-11-21 14:48:38 +0200

    add libccd and libfcl installtion scripts.

---
+++ ubuntu/install-libccd.sh
@@ -0,0 +1,9 @@
+
+#from: http://askubuntu.com/questions/727424/e-unable-to-locate-package-libccd-dev
+sudo add-apt-repository ppa:libccd-debs/ppa
+
+sudo apt-get update
+
+sudo apt-get install libccd-dev -y
+
+
---
+++ ubuntu/install-libfcl.sh
@@ -0,0 +1,10 @@
+
+#from: https://github.com/dartsim/dart/wiki/Linux-Installation-for-DART-4.2
+#from: https://www.howtoinstall.co/en/ubuntu/utopic/libfcl-dev
+sudo apt-add-repository ppa:fcl-debs
+
+sudo apt-get update
+
+sudo apt-get install libfcl-dev -y
+
+
