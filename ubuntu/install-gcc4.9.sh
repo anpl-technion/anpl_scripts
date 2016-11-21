67fae3088b87f37f5377e9a485b4bf27abeaba61
tal.regev@gmail.com <tal.regev@gmail.com>
2016-11-21 17:37:18 +0200

    update the gcc4.9 installation script.

--- ubuntu/install-gcc4.9.sh
+++ ubuntu/install-gcc4.9.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#from: http://askubuntu.com/questions/428198/getting-installing-gcc-g-4-9-on-ubuntu
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+
+sudo apt-get update
+
 #from http://charette.no-ip.com:81/programming/2011-12-24_GCCv47/
 sudo apt-get install gcc-4.9 g++-4.9 -y
 
