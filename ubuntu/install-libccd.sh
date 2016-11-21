675289161d79b02b288e89249de02b62eb2386ea
tal.regev@gmail.com <tal.regev@gmail.com>
2016-11-21 17:43:36 +0200

    update libccd and libfcl installation that will install from github from source

--- ubuntu/install-libccd.sh
+++ ubuntu/install-libccd.sh
@@ -1,9 +1,19 @@
+PREFIX=~/prefix
+PROJECT_NAME=libccd
+GIT_LINK=https://github.com/danfis/libccd
+PROJECT_DIR=~/ANPL/code/3rdparty
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX"
 
-#from: http://askubuntu.com/questions/727424/e-unable-to-locate-package-libccd-dev
-sudo add-apt-repository ppa:libccd-debs/ppa
 
-sudo apt-get update
+sudo rm -rf $PROJECT_DIR/$PROJECT_NAME
 
-sudo apt-get install libccd-dev -y
+cd $PROJECT_DIR/
+git clone $GIT_LINK
+cd $PROJECT_NAME
+mkdir build
+cd build
+cmake $CMAKE_FLAGS ..
+make -j4
+sudo make install -j4
 
 
--- ubuntu/install-libfcl.sh
+++ ubuntu/install-libfcl.sh
@@ -1,10 +1,22 @@
+PREFIX=~/prefix
+PROJECT_NAME=libfcl
+GIT_LINK=https://github.com/flexible-collision-library/fcl
+PROJECT_DIR=~/ANPL/code/3rdparty
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX"
 
-#from: https://github.com/dartsim/dart/wiki/Linux-Installation-for-DART-4.2
-#from: https://www.howtoinstall.co/en/ubuntu/utopic/libfcl-dev
-sudo apt-add-repository ppa:fcl-debs
+install-gcc4.9.sh
 
-sudo apt-get update
+#from https://github.com/flexible-collision-library/fcl
 
-sudo apt-get install libfcl-dev -y
+sudo rm -rf $PROJECT_DIR/$PROJECT_NAME
+
+cd $PROJECT_DIR/
+git clone $GIT_LINK $PROJECT_NAME
+cd $PROJECT_NAME
+mkdir build
+cd build
+cmake $CMAKE_FLAGS ..
+make -j4
+sudo make install -j4
 
 
