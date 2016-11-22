3eadc02afc67758653d384f8a845e89f7e11e9da
tal.regev@gmail.com <tal.regev@gmail.com>
2016-11-22 15:18:04 +0200

    update ompl app script. now standalone

--- ubuntu/install-ompl-app.sh
+++ ubuntu/install-ompl-app.sh
@@ -18,8 +18,17 @@ git clone $GIT_LINK $PROJECT_NAME
 cd $PROJECT_NAME
 git clone $SUB_GIT_LINK
 
+#Hack that make omplapp find libccd and libfcl
+cd ~/Downloads
+wget https://raw.githubusercontent.com/dartsim/dart/master/cmake/FindCCD.cmake
+wget https://raw.githubusercontent.com/dartsim/dart/master/cmake/FindFCL.cmake
+mv FindCCD.cmake FindFCL.cmake $PROJECT_DIR/$PROJECT_NAME/CMakeModules
+
+cd $PROJECT_DIR/$PROJECT_NAME
 mkdir build
 cd build
 cmake $CMAKE_FLAGS ..
+make -j4
+sudo make install -j4
 
 
