a47462f653d0013b94f7d10d843b320ebee8b9b9
tal.regev@gmail.com <tal.regev@gmail.com>
2017-01-10 13:49:48 +0200

    update libfcl script - now install v0.4. update pointclouds scirpt - now install v1.7.2 and v1.8.0.

--- ubuntu/install-libfcl.sh
+++ ubuntu/install-libfcl.sh
@@ -3,7 +3,7 @@ PROJECT_NAME=libfcl
 GIT_LINK=https://github.com/flexible-collision-library/fcl
 PROJECT_DIR=~/ANPL/code/3rdparty
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX"
-VERSION=0.5
+VERSION=0.4
 
 install-gcc4.9.sh
 
@@ -17,5 +17,5 @@ cd $PROJECT_NAME
 mkdir build
 cd build
 cmake $CMAKE_FLAGS ..
-make -j4
-sudo make install -j4
+make -j7
+sudo make install -j7
