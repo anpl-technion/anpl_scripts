534bcf716f897ec45ea318fbac1b43b1bc44a0a2
talregev <tal.regev@gmail.com>
2017-01-12 18:52:25 +0200

    update libccd and smartgit installation scripts.

--- ubuntu/install-libccd.sh
+++ ubuntu/install-libccd.sh
@@ -1,19 +1,22 @@
 PREFIX=~/prefix
-PROJECT_NAME=libccd
-GIT_LINK=https://github.com/danfis/libccd
 PROJECT_DIR=~/ANPL/code/3rdparty
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX"
+LIBCCD_VER=2.0
+FOLDER_NAME=libccd-$LIBCCD_VER
+FILE_NAME=$FOLDER_NAME.zip
 
+LINK=https://github.com/danfis/libccd/archive/v$LIBCCD_VER.zip
+PROJECT_DIR=~/ANPL/code/3rdparty
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
 
-sudo rm -rf $PROJECT_DIR/$PROJECT_NAME
-
-cd $PROJECT_DIR/
-git clone $GIT_LINK
-cd $PROJECT_NAME
-mkdir build
-cd build
+rm -rf $PROJECT_DIR/$FOLDER_NAME
+cd ~/Downloads
+wget -O $FILE_NAME $LINK
+mkdir -p $PROJECT_DIR
+unzip $FILE_NAME -d $PROJECT_DIR
+rm -f ~/Downloads/$FILE_NAME
+cd $PROJECT_DIR/$FOLDER_NAME
+mkdir build && cd build
 cmake $CMAKE_FLAGS ..
-make -j4
-sudo make install -j4
-
+make -j7       
+sudo make install -j7
 
--- ubuntu/install-smartgit.sh
+++ ubuntu/install-smartgit.sh
@@ -3,15 +3,19 @@
 VER='8_0_3'
 PROGRAMS=~/ANPL/programs
 WORKING_PATH=$PROGRAMS/smartgit/
+FILE_NAME=smartgit-$VER.tar.gz
+
+#install java
 install-java-jdk.sh
 
 # download file to Download folder
 rm -rf $WORKING_PATH
 cd ~/Downloads
-wget -O smartgit-$VER.tar.gz "http://www.syntevo.com/static/smart/download/smartgit/smartgit-linux-$VER.tar.gz"
-tar zxf smartgit-$VER.tar.gz -C $PROGRAMS
+wget -O $FILE_NAME "http://www.syntevo.com/static/smart/download/smartgit/smartgit-linux-$VER.tar.gz"
+mkdir -p $PROGRAMS
+tar zxf $FILE_NAME -C $PROGRAMS
 cd $WORKING_PATH/bin
 ./remove-menuitem.sh
 ./add-menuitem.sh
 
-rm -f ~/Downloads/smartgit-$VER.tar.gz
+rm -f ~/Downloads/$FILE_NAME
