31e5caac5eaaee9b3e977d1424901c992eca3633
talregev <tal.regev@gmail.com>
2017-01-12 19:03:52 +0200

    update libfcl installation script

--- ubuntu/install-libfcl.sh
+++ ubuntu/install-libfcl.sh
@@ -1,21 +1,25 @@
 PREFIX=~/prefix
-PROJECT_NAME=libfcl
-GIT_LINK=https://github.com/flexible-collision-library/fcl
 PROJECT_DIR=~/ANPL/code/3rdparty
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX"
-VERSION=0.4
+LIBFCL_VER=0.5.0
+FOLDER_NAME=libfcl-$LIBFCL_VER
+FILE_NAME=$FOLDER_NAME.zip
+
+LINK=https://github.com/flexible-collision-library/fcl/archive/$LIBFCL_VER.zip
+PROJECT_DIR=~/ANPL/code/3rdparty
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
 
 install-gcc4.9.sh
 
-#from https://github.com/flexible-collision-library/fcl
-
-sudo rm -rf $PROJECT_DIR/$PROJECT_NAME
-
+rm -rf $PROJECT_DIR/$FOLDER_NAME
+cd ~/Downloads
+wget -O $FILE_NAME $LINK
+mkdir -p $PROJECT_DIR
+unzip $FILE_NAME -d $PROJECT_DIR
+rm -f ~/Downloads/$FILE_NAME
 cd $PROJECT_DIR/
-git clone $GIT_LINK -b fcl-$VERSION $PROJECT_NAME
-cd $PROJECT_NAME
-mkdir build
-cd build
+mv fcl-$LIBFCL_VER $FOLDER_NAME
+cd $FOLDER_NAME
+mkdir build && cd build
 cmake $CMAKE_FLAGS ..
-make -j7
+make -j7       
 sudo make install -j7
