4894ca660e14763a4a3db42e4e68187b3eca6113
asafeniger <asafeniger@gmail.com>
2017-01-16 12:04:19 +0200

    - update installation script for planar icp  - add path to csm.pc to PKG_CONFIG_PATH

--- ubuntu/install-planar-icp.sh
+++ ubuntu/install-planar-icp.sh
@@ -2,15 +2,18 @@
 
 PROJECT_DIR=~/ANPL/code/3rdparty
 PREFIX=~/prefix
+FOLDER_NAME=planar_icp
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
 
+
+sudo rm -rf $PROJECT_DIR/$FOLDER_NAME
+
 cd $PROJECT_DIR
+git clone https://bitbucket.org/ANPL/planar_icp $FOLDER_NAME
+cd $FOLDER_NAME
 
-git clone https://bitbucket.org/ANPL/planar_icp
-
-cd planar_icp
-
-mkdir -p build-release
-cd build-release
+mkdir build && cd build
+PKG_CONFIG_PATH=~/prefix/lib/pkgconfig/
 cmake $CMAKE_FLAGS ..
-sudo make install
+make -j7
+sudo make install -j7
