edb4e0e7cda656670f8933541e8955028cabddf4
tal.regev@gmail.com <tal.regev@gmail.com>
2017-01-16 10:48:57 +0200

    remove install-modules.sh update gtsam and planar-icp installation scripts.

--- ubuntu/install-gtsam.sh
+++ ubuntu/install-gtsam.sh
@@ -3,49 +3,50 @@
 
 #check matlab version
 MATLAB_VER=`matlab -e | grep -E -o R[0-9]+[ab] |uniq`
-CMAKE_FLAGS=""
+
 PREFIX=~/prefix
 PROJECT_DIR=~/ANPL/code/3rdparty
-GTSAM_VER="3.2.1"
 FROM_GIT=True
+GTSAM_VER="3.2.1"
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
+LINK="https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam-$GTSAM_VER.zip"
+GIT_LINK="https://bitbucket.org/gtborg/gtsam/ -b fix/boost158gtsam3"
 
-install-modules.sh
+
+sudo apt-get install libboost-all-dev libtbb-dev -y
+
 install-gcc4.9.sh
 
 #if there is matlab install on the machine
 if [ ! -z "$MATLAB_VER" ]; then
 	#flags for matlab
-    CMAKE_FLAGS="-DGTSAM_INSTALL_MATLAB_TOOLBOX=ON -DMEX_COMMAND=/usr/local/MATLAB/$MATLAB_VER/bin/mex $CMAKE_FLAGS"
+    CMAKE_FLAGS="$CMAKE_FLAGS -DGTSAM_INSTALL_MATLAB_TOOLBOX=ON -DMEX_COMMAND=/usr/local/MATLAB/$MATLAB_VER/bin/mex"
 fi
-
-CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX $CMAKE_FLAGS"
 
 sudo rm -rf $PROJECT_DIR/gtsam-$GTSAM_VER
 
-if [ ! "$FROM_GIT" = True ]; then
+if [ "$FROM_GIT" = True ]; then
+    cd $PROJECT_DIR
+    git clone $GIT_LINK gtsam-$GTSAM_VER
+else
     # download file to Download folder
     cd ~/Downloads
-    wget -O gtsam-$GTSAM_VER.zip "https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam-$GTSAM_VER.zip"
+    wget -O gtsam-$GTSAM_VER.zip $LINK
     unzip gtsam-$GTSAM_VER.zip -d $PROJECT_DIR
     rm -f ~/Downloads/gtsam-$GTSAM_VER.zip
-else
-    cd $PROJECT_DIR
-    git clone https://bitbucket.org/gtborg/gtsam/ -b fix/boost158gtsam3 gtsam-$GTSAM_VER
 fi
 
 cd $PROJECT_DIR/gtsam-$GTSAM_VER
 
 #from https://collab.cc.gatech.edu/borg/gtsam/#quickstart
 
-mkdir build-release
-cd build-release
-sudo cmake $CMAKE_FLAGS ..
-sudo make install -j4
+mkdir build && cd build
+cmake $CMAKE_FLAGS ..
+make -j7
+sudo make install -j7
 
 echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:~/prefix/lib">>~/.bashrc
 if [ ! -z "$MATLAB_VER" ]; then
     #save matlab the path for gtsam toolbox
     sudo matlab -nodesktop -nosplash -r "addpath(genpath('$PREFIX/gtsam_toolbox'));savepath;exit;"
 fi
-
-install-gcc5.sh
