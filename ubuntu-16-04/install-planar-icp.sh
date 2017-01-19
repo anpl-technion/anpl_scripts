812804ab310b36c60aa21ce4053fd0989fde1598
asafeniger <asafeniger@gmail.com>
2017-01-19 15:42:51 +0200

    update csm and planar-icp scripts for ubuntu 16.04

--- ubuntu-16-04/install-planar-icp.sh
+++ ubuntu-16-04/install-planar-icp.sh
@@ -5,8 +5,6 @@ PREFIX=/usr/ANPLprefix
 FOLDER_NAME=planar_icp
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
 
-install-csm.sh
-
 sudo rm -rf $PROJECT_DIR/$FOLDER_NAME
 
 cd $PROJECT_DIR
@@ -14,7 +12,7 @@ git clone https://bitbucket.org/ANPL/planar_icp $FOLDER_NAME
 cd $FOLDER_NAME
 
 mkdir build && cd build
-PKG_CONFIG_PATH=$PKG_CONFIG_PATH:~/prefix/lib/pkgconfig/
+PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PREFIX/lib/pkgconfig/
 cmake $CMAKE_FLAGS ..
 make -j7
 sudo make install -j7
