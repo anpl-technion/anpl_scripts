6b9e7ce669e949622f27ec61ce604491c9267a32
asafeniger <asafeniger@gmail.com>
2017-01-24 11:25:09 +0200

    update gtsam-toolbox script

--- ubuntu-16-04/install-toolbox-gtsam.sh
+++ ubuntu-16-04/install-toolbox-gtsam.sh
@@ -3,13 +3,17 @@ PREFIX=/usr/ANPLprefix
 GTSAM_TOOLBOX_VER="3.2.0"
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
 LINK="https://research.cc.gatech.edu/borg/sites/edu.borg/files/downloads/gtsam-toolbox-$GTSAM_TOOLBOX_VER-lin64.tgz"
-FILE_NAME=gtsam-toolbox-$GTSAM_VER.zip
+FILE_NAME=gtsam-toolbox-$GTSAM_TOOLBOX_VER.tgz
+FOLDER_NAME=gtsam_toolbox
 
+sudo rm -rf $PREFIX/$FOLDER_NAME
 
 cd ~/Downloads
 wget -O $FILE_NAME $LINK
-unzip $FILE_NAME -d $PREFIX
+tar zxvf $FILE_NAME
+sudo mv $FOLDER_NAME $PREFIX
+rm -rf ~/Downloads/$FILE_NAME
 
 #save matlab the path for gtsam toolbox
-sudo matlab -nodesktop -nosplash -r "addpath(genpath('$PREFIX/gtsam_toolbox'));savepath;exit;"
+sudo matlab -nodesktop -nosplash -r "addpath(genpath('$PREFIX/$FOLDER_NAME'));savepath;exit;"
 
