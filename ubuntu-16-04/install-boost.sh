96baeef28554f48bfaba4d50cc352dfbe8c6601c
talregev <tal.regev@gmail.com>
2017-06-21 15:45:12 +0300

    update boost script. still need to update.

--- ubuntu-16-04/install-boost.sh
+++ ubuntu-16-04/install-boost.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PREFIX=/usr/ANPL/gtsam_toolbox_prefix
+PREFIX=/usr/ANPLprefix
 PROJECT_DIR=~/ANPL/code/3rdparty
 BOOST_VER=1.58.0
 BOOST_VER_STR=`echo $BOOST_VER | tr . _`
@@ -9,10 +9,8 @@ FILE_NAME=$FOLDER_NAME.zip
 
 LINK=https://netix.dl.sourceforge.net/project/boost/boost/$BOOST_VER/boost_$BOOST_VER_STR.zip
 PROJECT_DIR=~/ANPL/code/3rdparty
-BOOST_FLAGS="--prefix=$PREFIX"
+BOOTSTRAP_FLAGS="--prefix=$PREFIX"
 B2_FLAGS="link=static threading=multi cxxflags=-fPIC cflags=-fPIC --disable-icu -j8"
-
-install-gcc4.9.sh
 
 sudo mkdir -p $PREFIX
 sudo rm -rf $PROJECT_DIR/$FOLDER_NAME
@@ -23,8 +21,6 @@ unzip $FILE_NAME -d $PROJECT_DIR
 rm -f ~/Downloads/$FILE_NAME
 cd $PROJECT_DIR/$FOLDER_NAME
 
-./bootstrap.sh $BOOST_FLAGS
+./bootstrap.sh $BOOTSTRAP_FLAGS
 ./b2 $B2_FLAGS
 sudo ./b2 install -j8
-
-install-gcc5.sh
