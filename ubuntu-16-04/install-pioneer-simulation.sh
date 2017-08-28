aa72c2b0f1b07bfbc8f3a7571ff17978906d2884
asafeniger <asafeniger@gmail.com>
2017-08-28 11:49:55 +0300

    - add script to install pionner simulation in gazebo for ubuntu 16

---
+++ ubuntu-16-04/install-pioneer-simulation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CATKIN_WS_SRC=~/ANPL/infrastructure/catkin_ws/src
+
+cd $CATKIN_WS_SRC
+
+# download pioneer desccription repo
+git clone https://github.com/MobileRobots/amr-ros-config
+
+# download pioneer keyboard controller repo
+git clone https://bitbucket.org/ANPL/pioneer_keyop
+
+cd ..
+catkin_make
