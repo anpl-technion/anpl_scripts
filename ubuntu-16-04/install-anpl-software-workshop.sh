4ac08056ac719952676292464804d657c8b0f305
talregev <tal.regev@gmail.com>
2018-07-08 14:57:41 +0300

    remove old scripts update mavros script update install mrbsp workspace update ubuntu16 installation guide add ros tools scripts

--- ubuntu-16-04/install-anpl-software-workshop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MRBSP_WS=~/ANPL/code/mrbsp_ws
-
-cd $MRBSP_WS/src
-git clone https://bitbucket.org/ANPL/anpl_software_workshop
-cd ..
-catkin build
-
--- ubuntu-16-04/install-mrbsp_ros-ws.sh
+++ ubuntu-16-04/install-mrbsp_ros-ws.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-CATKIN_WS_NAME=mrbsp_ws
-PATH_TO_WS=~/ANPL/code/$CATKIN_WS_NAME
+PROJECT_NAME=mrbsp_ros
+WS_NAME=mrbsp_ws
+WS_PATH=~/ANPL/infrastructure/$WS_NAME
+PREFIX=/usr/ANPLprefix
 
-sudo apt-get install python-catkin-tools -y
+#remove old mrbsp_ros workspace
+sudo rm -rf $WS_PATH
 
-cd
-mkdir -p $PATH_TO_WS/src && cd $PATH_TO_WS
+mkdir -p $WS_PATH/src && cd $WS_PATH
+catkin init
 catkin build
-echo "source $PATH_TO_WS/devel/setup.bash" >> ~/.bashrc
-source $PATH_TO_WS/devel/setup.bash
 
-cd src
-git clone https://bitbucket.org/ANPL/mrbsp_ros
-git clone https://bitbucket.org/ANPL/anpl_ros_infrastructur anpl_inf
-git clone https://github.com/MobileRobots/amr-ros-config
+source $WS_PATH/devel/setup.bash
+echo "source $WS_PATH/devel/setup.bash" >> ~/.bashrc
+
+cd $WS_PATH/src
 git clone https://bitbucket.org/ANPL/pioneer_keyop
+git clone https://bitbucket.org/ANPL/anpl_ros_infrastructur anpl_inf
+git clone https://bitbucket.org/ANPL/mrbsp_ros
+git clone https://github.com/MobileRobots/amr-ros-config
 
-cd mrbsp_ros/mrbs_ros_utils/scripts
+#copy cmake files that node will find anpl libs.
+cd $WS_PATH/src/mrbsp_ros/mrbsp_ros_utils/scripts/
 sudo sh install-find-cmakes.sh
 sudo sh install-diverce-short-path.sh
 
-sudo apt-get install ros-kinetic-ecl -y
-sudo apt-get install ros-kinetic-rviz-visual-tools -y
 
-cd $PATH_TO_WS
+cd $WS_PATH
 catkin build
+
+
 
 
 
