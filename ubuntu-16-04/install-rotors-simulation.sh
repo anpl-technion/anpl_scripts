bdb1cf3e2198f98561fca40a54af5e6757782f4d
talregev <tal.regev@gmail.com>
2018-07-22 18:45:48 +0300

    typo fix in rotors-simulation.

--- ubuntu-16-04/install-rotors-simulation.sh
+++ ubuntu-16-04/install-rotors-simulation.sh
@@ -8,8 +8,8 @@ rm -rf $CATKIN_WS/src/$MAV_DIR
 rm -rf $CATKIN_WS/src/$ROTOR_DIR
 
 sudo apt-get install autoconf -y
-sudo apt install protobuf-compiler -y
-sudo apt install libgoogle-glog-dev -y
+sudo apt-get install protobuf-compiler -y
+sudo apt-get install libgoogle-glog-dev -y
 
 
 cd $CATKIN_WS/src
@@ -17,7 +17,7 @@ cd $CATKIN_WS/src
 git clone https://github.com/ethz-asl/mav_comm.git $MAV_DIR
 git clone --recursive https://github.com/ethz-asl/rotors_simulator.git $ROTOR_DIR
 
-#delete rotors_hil_interface because we didn't figure yet how to compile it. 
+#delete rotors_hil_interface because we didn't figure out yet how to compile it. 
 cd $CATKIN_WS/src/$ROTOR_DIR
 rm -rf rotors_hil_interface/
 
