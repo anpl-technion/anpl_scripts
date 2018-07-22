c1afebb1c61ecbb09377b2662ac9e10babf70981
talregev <tal.regev@gmail.com>
2018-07-22 14:22:36 +0300

    upate mrbsp ws: install rosaria, one of dependent for run real experiment

--- ubuntu-16-04/install-mrbsp_ros-ws.sh
+++ ubuntu-16-04/install-mrbsp_ros-ws.sh
@@ -24,7 +24,7 @@ git clone https://github.com/MobileRobots/amr-ros-config
 #copy cmake files that node will find anpl libs.
 cd $WS_PATH/src/mrbsp_ros/mrbsp_ros_utils/scripts/
 sudo sh install-find-cmakes.sh
-
+install-rosaria.sh
 
 cd $WS_PATH
 catkin build
