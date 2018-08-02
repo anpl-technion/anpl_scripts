cbcf134fc54378daf8d911fd61d763af57b10410
talregev <tal.regev@gmail.com>
2018-08-02 11:36:42 +0300

    update qgroundcontrol script.

--- ubuntu-16-04/install-qgroundcontrol.sh
+++ ubuntu-16-04/install-qgroundcontrol.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ANPL_OPT=/opt/ANPL
-QG_VER=3.3.2
-QG_DIR=$ANPL_OPT/qgroundcontrol
-LINK=https://github.com/mavlink/qgroundcontrol/releases/download/v$QG_VER/QGroundControl.AppImage
+QGC_VER=3.3.2
+QGC_DIR=$ANPL_OPT/qgroundcontrol
+LINK=https://github.com/mavlink/qgroundcontrol/releases/download/v$QGC_VER/QGroundControl.AppImage
 LINK_ICON=https://github.com/mavlink/qgroundcontrol/raw/master/resources/icons/android_512x512.png
 
 
@@ -15,22 +15,23 @@ cd ~/Downloads
 wget -O QGroundControl.AppImagee $LINK
 wget -O QGroundControl_512x512.png $LINK_ICON
 chmod +x QGroundControl.AppImagee
-sudo mkdir -p $QG_DIR
-sudo mv QGroundControl.AppImagee QGroundControl_512x512.png $CURA_DIR
+sudo mkdir -p $QGC_DIR
+sudo mv QGroundControl.AppImagee QGroundControl_512x512.png $QGC_DIR
 
 
 cd ~/Downloads
 cat << EOF > QGroundControl.desktop
 [Desktop Entry]
-Version=$CURA_VER
+Version=$QGC_VER
 Type=Application
 Name=QGroundControl
-Icon=$QG_DIR/QGroundControl_512x512.png
-Exec=bash -i -c "$QG_DIR/QGroundControl.AppImagee" %f
+Icon=$QGC_DIR/QGroundControl_512x512.png
+Exec=bash -i -c "$QGC_DIR/QGroundControl.AppImagee" %f
 Comment=Ground Control Station
-Categories=GCS;
+Categories=GCS;QGC;
 Terminal=false
 StartupWMClass=QGroundControl
 EOF
 
 sudo mv QGroundControl.desktop /usr/share/applications
+sudo usermod -a -G dialout $USER
