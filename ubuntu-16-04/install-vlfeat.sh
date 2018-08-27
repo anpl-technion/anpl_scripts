8e72e336f8b4aa4e4c8a8e74e66a4d2c446f3aa4
talregev <tal.regev@gmail.com>
2018-08-27 18:50:38 +0300

    update vlfeat installation script

--- ubuntu-16-04/install-vlfeat.sh
+++ ubuntu-16-04/install-vlfeat.sh
@@ -2,29 +2,19 @@
 
 #from http://www.vlfeat.org/compiling-unix.html
 #from http://www.vlfeat.org/download.html
+#from https://github.com/vlfeat/vlfeat
 
-#need install gcc4.7 for compiling with matlab
-install-gcc4.9.sh
-
-#check matlab version
-MATLAB_VER=`matlab -e | grep -E -o R[0-9]+[ab] |uniq`
-MAKE_FLAGS=""
-PREFIX=/usr/ANPLprefix
+VLFEAT_VER=0.9.21
 PROJECT_DIR=~/ANPL/code/3rdparty
-INSTALL_DIR=$PROJECT_DIR/vlfeat/toolbox
+FILE_NAME=vlfeat-$VLFEAT_VER.tar.gz
+LINK=http://www.vlfeat.org/download/vlfeat-$VLFEAT_VER-bin.tar.gz
+INSTALL_DIR=$PROJECT_DIR/vlfeat-$VLFEAT_VER/toolbox
 
-#if there is no matlab install on the machine
-if [ ! -z "$MATLAB_VER" ]; then
-	#flags for matlab
-    MAKE_FLAGS="MEX=/usr/local/MATLAB/$MATLAB_VER/bin/mex $MAKE_FLAGS"
-fi
+sudo rm -rf $PROJECT_DIR/vlfeat-*
+cd ~/Downloads
+wget -O $FILE_NAME $LINK
+tar -xvzf $FILE_NAME
+mv vlfeat-$VLFEAT_VER $PROJECT_DIR
+rm -rf $FILE_NAME
 
-rm -rf $PROJECT_DIR/vlfeat
-cd $PROJECT_DIR
-git clone git://github.com/vlfeat/vlfeat.git
-cd vlfeat
-make $MAKE_FLAGS
-
-sudo matlab -nodesktop -nosplash -r "addpath(genpath('$INSTALL_DIR'));run('$INSTALL_DIR/vl_setup');savepath;exit;"
-
-install-gcc5.sh
+sudo matlab -nodesktop -nosplash -r "run('$INSTALL_DIR/vl_setup');savepath;exit;"
