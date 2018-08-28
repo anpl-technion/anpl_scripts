7da1b7c4a6f743785d935373267624fefeae033f
talregev <tal.regev@gmail.com>
2018-08-28 13:58:11 +0300

    add desktop icon to matlab.

--- ubuntu-16-04/install-matlab-network.sh
+++ ubuntu-16-04/install-matlab-network.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #from https://help.ubuntu.com/community/MountIso
+LINK_ICON=https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png
 
 sudo apt-get install nfs-common -y
 
@@ -12,6 +13,21 @@ SERVER lm.technion.ac.il 001b24e02dd5 1700
 USE_SERVER
 EOF
 sudo mv network.lic /tmp/network
+sudo wget $LINK_ICON -O /usr/share/icons/matlab.png
+
+cat << EOF > matlab.desktop
+[Desktop Entry]
+Version=1.0
+Type=Application
+Name=Matlab
+Icon=/usr/share/icons/matlab.png
+Exec=bash -i -c "/usr/local/bin/matlab -desktop"
+Comment=Start MATLAB - The Language of Technical Computing
+Categories=Development;IDE;
+Terminal=false
+StartupWMClass=matlab
+EOF
+sudo mv matlab.desktop /usr/share/applications
 cd /media/matlab
 sudo ./install
 sleep 2
