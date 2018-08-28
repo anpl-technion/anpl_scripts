bc651fe3494a7371579997444f21cef8b612ac28
talregev <tal.regev@gmail.com>
2018-08-28 11:19:25 +0300

    update cuda installation scripts

--- ubuntu-16-04/set-user-bashrc.sh
+++ ubuntu-16-04/set-user-bashrc.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 # gtsam
-echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/ANPLprefix/lib">>~/.bashrc
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/ANPLprefix/lib" >>~/.bashrc
+
+# cuda
+echo "export PATH=\$PATH:/usr/local/cuda/bin/" >>~/.bashrc
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda/lib64" >>~/.bashrc
 
 # ros kinetic
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /opt/ros/kinetic/setup.bash" >>~/.bashrc
+
+
+
+
