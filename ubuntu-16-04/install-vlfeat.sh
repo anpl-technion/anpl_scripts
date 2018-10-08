56c069f4f9ebcd26c0a56a3035a5be15470ea287
talregev <tal.regev@gmail.com>
2018-10-08 09:31:49 +0300

    put vlfeat in anpl/prefix for all user installtion.

--- ubuntu-16-04/install-vlfeat.sh
+++ ubuntu-16-04/install-vlfeat.sh
@@ -5,7 +5,7 @@
 #from https://github.com/vlfeat/vlfeat
 
 VLFEAT_VER=0.9.21
-PROJECT_DIR=~/ANPL/code/3rdparty
+PROJECT_DIR=/usr/ANPL/prefix
 FILE_NAME=vlfeat-$VLFEAT_VER.tar.gz
 LINK=http://www.vlfeat.org/download/vlfeat-$VLFEAT_VER-bin.tar.gz
 INSTALL_DIR=$PROJECT_DIR/vlfeat-$VLFEAT_VER/toolbox
@@ -14,7 +14,7 @@ sudo rm -rf $PROJECT_DIR/vlfeat-*
 cd ~/Downloads
 wget -O $FILE_NAME $LINK
 tar -xvzf $FILE_NAME
-mv vlfeat-$VLFEAT_VER $PROJECT_DIR
+sudo mv vlfeat-$VLFEAT_VER $PROJECT_DIR
 rm -rf $FILE_NAME
 
 sudo matlab -nodesktop -nosplash -r "run('$INSTALL_DIR/vl_setup');savepath;exit;"
