e9644ddb5ac1958dc5eac8b349c11b87a8694614
talregev <tal.regev@gmail.com>
2018-10-08 13:27:59 +0300

    complie lib pcl with cuda. move orbslam to share area: usr/anpl/prefix. for all user uses.

--- ubuntu-16-04/install-orbslam2.sh
+++ ubuntu-16-04/install-orbslam2.sh
@@ -2,6 +2,7 @@
 
 PREFIX=/usr/ANPLprefix
 PROJECT_DIR=~/ANPL/code/3rdparty
+ANPL_PREFIX=/usr/ANPL/prefix
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release"
 FOLDER_NAME_PANGOLIN=pangolin
 FOLDER_NAME_ORB_SLAM_2=orb-slam2
@@ -11,7 +12,7 @@ MAKE_FLAGS=-j7
 
 # Install ORB SLAM2 and it's dependencies
 #install opencv 3
-install-opencv3.sh
+#install-opencv3.sh
 
 # install libglew (for pangolin)
 sudo apt-get install libglew-dev -y
@@ -29,31 +30,31 @@ sudo make install $MAKE_FLAGS
 
 
 #delete old orb-slam2 library
-sudo rm -rf $PROJECT_DIR/$FOLDER_NAME_ORB_SLAM_2
+sudo rm -rf $ANPL_PREFIX/$FOLDER_NAME_ORB_SLAM_2
 
-cd $PROJECT_DIR
-git clone $LINK_ORB_SLAM_2 $FOLDER_NAME_ORB_SLAM_2
+cd $ANPL_PREFIX
+sudo git clone $LINK_ORB_SLAM_2 $FOLDER_NAME_ORB_SLAM_2
 cd $FOLDER_NAME_ORB_SLAM_2
 cd Thirdparty/DBoW2
-mkdir build && cd build
-cmake $CMAKE_FLAGS ..
-make $MAKE_FLAGS
+sudo mkdir build && cd build
+sudo cmake $CMAKE_FLAGS ..
+sudo make $MAKE_FLAGS
 
 echo "Configuring and building Thirdparty/g2o ..."
 cd ../../g2o
-mkdir build && cd build
-cmake $CMAKE_FLAGS ..
-make $MAKE_FLAGS
+sudo mkdir build && cd build
+sudo cmake $CMAKE_FLAGS ..
+sudo make $MAKE_FLAGS
 
 
 echo "Uncompress vocabulary ..."
 cd ../../../Vocabulary
-tar -xf ORBvoc.txt.tar.gz
+sudo tar -xf ORBvoc.txt.tar.gz
 cd ..
 
 echo "Configuring and building ORB_SLAM2 ..."
 
-mkdir build && cd build
-cmake $CMAKE_FLAGS ..
-make $MAKE_FLAGS
+sudo mkdir build && cd build
+sudo cmake $CMAKE_FLAGS ..
+sudo make $MAKE_FLAGS
 
