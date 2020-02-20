a36dc122daf4258a45cc6ad0978d31b09b7e7e45
melnikovg <melnikovg97@gmail.com>
2020-02-20 15:30:05 +0200

    minimal installation script

--- grigory-installator/ubuntu18_JenTest/src/install-ompl.sh
+++ grigory-installator/ubuntu18_JenTest/src/install-ompl.sh
@@ -16,6 +16,9 @@ FROM_APT=True
 if [ $FROM_APT = True ]; then
     # sudo apt-get autoremove libompl-dev 
     sudo apt-get install libompl-dev -y
+
+    cd usr/share
+    sudo cp ompl/ompl-config.cmake ompl/FindOMPL.cmake
     exit
 fi
 
