078093feb974c500327380ad9afa57a8d6d2a5c5
jeniabb <evgenykoretsky@gmail.com>
2020-04-19 16:13:07 +0300

    showgitbranch text

--- grigory-installator/ubuntu16.04/helper_tester.sh
+++ grigory-installator/ubuntu16.04/helper_tester.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install ros-$ROS_DISTRO-octomap* -y && wait $! && TMP=1
-if [ $TMP -eq 1 ]; then
-    echo OK
-else
-    echo FAIL
-fi 
-echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-wait $!
-echo "---------------------------------------------------"
-    sleep 5
-echo "5555555555555555555555555555555555555555555555555555"
 
-
-echo is it done?
+echo "Is it done?"
 
