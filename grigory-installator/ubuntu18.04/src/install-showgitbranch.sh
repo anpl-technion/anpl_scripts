74d7721141858ba70ab5734df6613482f6fbfa99
ANPL <anpl15@aevadim15.ae.technion.ac.il>
2020-05-27 13:10:04 +0300

    add showbranchname

---
+++ grigory-installator/ubuntu18.04/src/install-showgitbranch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+####################################################################
+MY_PATH="`dirname \"$0\"`"              # relative
+MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
+if [ -z "$MY_PATH" ] ; then
+  # error; for some reason, the path is not accessible
+  # to the script (e.g. permissions re-evaled after suid)
+  exit 1  # fail
+fi
+####################################################################
+
+case `grep -Fxq "# Show git branch name" ~/.bashrc >/dev/null; echo $?` in
+  0)
+    read -p "Show git branch name already exists in '~/.bashrc', press enter to continue" NULL
+    ;;
+  1)
+    cat $MY_PATH/showgitbranchname.txt >> ~/.bashrc
+    exec bash
+    # echo $MY_PATH
+    read -p "Added showgitbranchname in bashrc, press enter to continue" NULL
+    
+    ;;
+  *) 
+    read -p "Error occurred, press enter to continue" NULL
+    ;;
+esac
+
+
+
