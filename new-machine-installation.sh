2e3a2cdcf23f7697c28a623d13c622c80f989e7c
Anton Gulyaev <lynnkse@gmail.com>
2021-04-21 17:00:24 +0300

    infrastructure choise disabled

--- new-machine-installation.sh
+++ new-machine-installation.sh
@@ -151,46 +151,19 @@ while true; do
     esac
 done
 
-echo -e $'\033[0;42m Choosing Infrastructure \033[0m'
+PROJECT_NAME=anpl_mrbsp
+echo -e "\033[0;42m Choosing Branch \033[0m"
 while true; do
-	read -p $'Choose which infrastructure you want: \n\t1. anpl_mrbsp[NEW]\n\t2. mrbsp_ros[OLD]\nChoose an option: ' NUM
+	read -p $'Choose which branch you want: \n\t1. master[Lidar-gtsam3]\n\t2. gtsam4[Lidar-gtsam4]\nChoose an option: ' NUM
 	case $NUM in
-		[1] ) PROJECT_NAME=anpl_mrbsp
-			echo -e "\033[0;42m Choosing Branch \033[0m"
-			while true; do
-				read -p $'Choose which branch you want: \n\t1. master[Lidar-gtsam3]\n\t2. gtsam4[Lidar-gtsam4]\nChoose an option: ' NUM
-				case $NUM in
-					[1] ) BRANCH=master
-						GTSAM_VER=3;;
-					[2] ) BRANCH=gtsam4
-						GTSAM_VER=4;;
-				esac
-				case $NUM in
-					[12] ) break;;
-					* ) echo -e "\033[0;41m Please choose correct option.\033[0m";;
-				esac
-			done
-			break
-			;;
-		[2] ) PROJECT_NAME=mrbsp_ros
-			echo -e "\033[0;42m Choosing Branch \033[0m"
-			while true; do
-				read -p $'Choose which branch you want: \n\t1. t-bsp-julia[Lidar]\n\t2. or-vi_project[VISION] \nChoose an option: ' NUM
-				GTSAM_VER=3
-				case $NUM in
-					[1] ) BRANCH=t-bsp-julia;;
-					[2] ) BRANCH=or-vi_project
-						VISION=true;;
-				esac
-				case $NUM in
-					[12] ) break;;
-					* ) echo -e "\033[0;41m Please choose correct option.\033[0m";;
-				esac
-			done
-			break
-			;;
-		*) echo -e "\033[0;41m Please choose correct option.\033[0m"
-			;;
+		[1] ) BRANCH=master
+			GTSAM_VER=3;;
+		[2] ) BRANCH=gtsam4
+			GTSAM_VER=4;;
+	esac
+	case $NUM in
+		[12] ) break;;
+		* ) echo -e "\033[0;41m Please choose correct option.\033[0m";;
 	esac
 done
 
